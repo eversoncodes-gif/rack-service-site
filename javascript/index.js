ce8953a0c27d64fe5bffa8c3ba382cb261cca1b1
Everson Muianga <eversoncodes@gmail.com>
2026-06-04 23:12:34 +0200

    more details about the message.

--- javascript/index.js
+++ javascript/index.js
@@ -1,2 +1,7 @@
-alert("")
-document.getElementById
+//auqi sao as variaveis, nao se esqueca o querySelector e para selecionar um item dentro do html
+const menu = document.querySelector(".menu-toggle");
+const nav = document.querySelector(".nav-bar");
+
+menu.addEventListener("click",() =>{
+  nav.classList.toggle("active");
+});
