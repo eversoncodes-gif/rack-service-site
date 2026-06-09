e84acb5a1c2199e501e334d25c547639e4c64912
Everson Muianga <eversoncodes@gmail.com>
2026-06-09 22:18:29 +0200

    criei um menu mobil ainda nao esta completo

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,9 @@
-function abrirMenu(){
-  let menu = document.getElementsById(".menu-toggle");
-  if (menu.style.display==="none") {menu.style.display= "block";    
-  }
-  else{menu.style.display="none"}
-}
+ // aqui sao as variaveis constantes
+ const menu = document.querySelector(".menu-toggle")
+ const nav = document.querySelector(".nav-bar")
 
-
-
-// // aqui sao as variaveis constantes
-// const menu = document.querySelector(".menu-toggle")
-// const nav = document.querySelector(".nav-bar")
-
-// // aqui e o evento que quero que aconteca
-// menu.addEventListener("click", () =>{
-//   nav.classList.toggle("active")
-// })
+ // aqui e o evento que quero que aconteca
+menu.addEventListener("click", () =>{
+  nav.classList.toggle("active")
+ })
 
